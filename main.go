e8f61f7fcc9b09c4568b8bdfd937a68aed7c4495
Sayantan Saha <sayantan.somu@gmail.com>
2024-03-12 15:00:51 +0530

    moved to gin framework

--- main.go
+++ main.go
@@ -1,85 +1,88 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/fogleman/gg"
-	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("No .env file found")
+var (
+	// Configuration variables
+	listenPort = getEnv("PORT", "8080")
+	fontSize   = parseFloatEnv("FONT_SIZE", 24)
+	fontPath   = getEnv("FONT_PATH", "./HackNerdFont-Bold.ttf")
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/text-to-image/:text", textToImageHandler)
+	// r.GET("/font-as-bytes", fontAsBytesHandler)
+
+	//logMessage(fmt.Sprintf("Listening on port %s", listenPort))
+	if err := r.Run(":" + listenPort); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
-func textToImageHandler(w http.ResponseWriter, r *http.Request, fontSize float64, fontPath string) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
 
-	// Extracting text from the URL path, assuming the URL is like /text-to-image/{text}
-	// Split the URL path and extract the text part
-	parts := strings.SplitN(r.URL.Path, "/text-to-image/", 2)
-	if len(parts) != 2 || parts[1] == "" {
-		http.Error(w, "Text must be provided in the URL path", http.StatusBadRequest)
-		return
-	}
-	text := parts[1]
+func textToImageHandler(c *gin.Context) {
+	text := c.Param("text")
+	//logRequest(c.Request, text)
 
-	// Create an image with the text with a transparent background
-
-	const padding = 20
 	dc := gg.NewContext(100, 100)
-	width, height := dc.MeasureString(text)
-	width += 2 * padding  // Add padding to the width
-	height += 2 * padding // Add padding to the height
-
 	if err := dc.LoadFontFace(fontPath, fontSize); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		c.String(http.StatusInternalServerError, "Failed to load font face")
 		return
 	}
-	dc = gg.NewContext(int(width), int(height)) // Adding a little padding
-	dc.SetRGBA(0, 0, 0, 0)                      // Transparent background
+
+	width, height := dc.MeasureString(text)
+	dc = gg.NewContext(int(width)+10, int(height)+10) // Add some padding
+	dc.SetRGBA(0, 0, 0, 0)                            // Transparent background
 	dc.Clear()
 	dc.SetRGB(0, 0, 0) // Black text
 	dc.DrawStringAnchored(text, width/2, height/2, 0.5, 0.5)
 	dc.Clip()
 
-	// Write the image to the response
-	w.Header().Set("Content-Type", "image/png")
-	dc.EncodePNG(w)
+	//logResponse(text, int(width)+10, int(height)+10)
+
+	buf := new(bytes.Buffer)
+	if err := dc.EncodePNG(buf); err != nil {
+		c.String(http.StatusInternalServerError, "Failed to encode image")
+		return
+	}
+
+	c.Data(http.StatusOK, "image/png", buf.Bytes())
 }
 
-func main() {
-	// Simple dynamic route handling
-	port, exists := os.LookupEnv("PORT")
-	if !exists {
-		port = "8080" // Default port
-	}
+// func logRequest(r *http.Request, text string) {
+// 	logMessage(fmt.Sprintf("Request: %s, Path: %s", text, r.URL.Path))
+// }
 
-	fontSizeStr, exists := os.LookupEnv("FONTSIZE")
-	if !exists {
-		fontSizeStr = "24" // Default font size
-	}
-	fontSize, _ := strconv.ParseFloat(fontSizeStr, 64)
+// func logResponse(text string, width, height int) {
+// 	logMessage(fmt.Sprintf("Response: %s, Image Size: %dx%d", text, width, height))
+// }
 
-	fontPath, exists := os.LookupEnv("FONTPATH")
-	if !exists {
-		fontPath = "./HackNerdFont-Bold.ttf" // Default font path
+// func logMessage(message string) {
+// 	log.Printf("[%s] %s", time.Now().Format("2006-01-02 15:04:05"), message)
+// }
+
+func getEnv(key, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasPrefix(r.URL.Path, "/text-to-image/") {
-			textToImageHandler(w, r, fontSize, fontPath)
-		} else {
-			http.NotFound(w, r)
+	return fallback
+}
+
+func parseFloatEnv(key string, fallback float64) float64 {
+	if value, exists := os.LookupEnv(key); exists {
+		if floatValue, err := strconv.ParseFloat(value, 64); err == nil {
+			return floatValue
 		}
-	})
-
-	http.ListenAndServe(":"+port, nil)
+	}
+	return fallback
 }
